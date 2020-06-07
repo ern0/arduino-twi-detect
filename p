eaeb6bda008d18d5bfaa4e27b3cab296fa7bed27
ern0 <ern0@linkbroker.hu>
2020-06-07 23:35:57 +0200

    transport: pio set up

---
+++ p
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pio run -e arduino ; exit
+pio run -t upload -e arduino
---
+++ src/detect.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+
+	Serial.begin(38400);
+
+} // setup()
+
+
+void loop() {
+
+} // loop()
