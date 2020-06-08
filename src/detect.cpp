6a1be342c97cfb098853d6e07710edae4a68a1e9
ern0 <ern0@linkbroker.hu>
2020-06-08 14:06:02 +0200

    added extra scripts

--- src/detect.cpp
+++ src/detect.cpp
@@ -1,12 +1,22 @@
 #include <Arduino.h>
+#include <Wire.h>
 
 void setup() {
 
+	Wire.begin();
 	Serial.begin(38400);
+
+	Serial.println("I2C/TWI scanner");
 
 } // setup()
 
 
 void loop() {
+
+	for (int address = 0; address < 128; address++) {
+
+
+
+	} // for address
 
 } // loop()
